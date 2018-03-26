443b61631221844c8294a82471cbe0574ff93c43
kirypto <kirypto@gmail.com>
2018-03-25 19:11:30 -0600

    Working on implementing insertion into WatArrays

--- test/types/watArrays.test.js
+++ test/types/watArrays.test.js
@@ -31,4 +31,92 @@ describe("Wat Arrays", function () {
         // Assert
         expect(actual).toBe(0);
     });
+
+    it('WatArray.__(array,index,value) :: insert :: should false when negative index provided', function () {
+        // Arrange
+        let array = WatArray();
+
+        // Act
+        let actual = WatArray.__(array, -1, 4);
+
+        // Assert
+        expect(actual).toBe(false);
+    });
+
+    it('array.__(array,index,value) :: insert :: should false when negative index provided', function () {
+        // Arrange
+        let array = WatArray();
+
+        // Act
+        let actual = array.__(-1, 4);
+
+        // Assert
+        expect(actual).toBe(false);
+    });
+
+    it('WatArray.__(array,index,value) :: insert :: should return false when index is out of range', function () {
+        // Arrange
+        let array = WatArray();
+
+        // Act
+        let actual = WatArray.__(array, 1, 4);
+
+        // Assert
+        expect(actual).toBe(false);
+    });
+
+    it('array.__(array,index,value) :: insert :: should return false when index is out of range', function () {
+        // Arrange
+        let array = WatArray();
+
+        // Act
+        let actual = array.__(1, 4);
+
+        // Assert
+        expect(actual).toBe(false);
+    });
+
+    it('WatArray.__(array,index,value) :: insert :: should true when index is zero of new array', function () {
+        // Arrange
+        let array = WatArray();
+
+        // Act
+        let actual = WatArray.__(array, 0, 4);
+
+        // Assert
+        expect(actual).toBe(true);
+    });
+
+    it('array.__(array,index,value) :: insert :: should true when index is zero of new array', function () {
+        // Arrange
+        let array = WatArray();
+
+        // Act
+        let actual = array.__(0, 4);
+
+        // Assert
+        expect(actual).toBe(true);
+    });
+
+    it('WatArray.__(array,index,value) :: insert :: should contain value into position zero of new array', function () {
+        // Arrange
+        let array = WatArray();
+
+        // Act
+        let actual = WatArray.__(array, 0, 4);
+
+        // Assert
+        expect(actual[0]).toBe(4);
+    });
+
+    it('array.__(array,index,value) :: insert :: should contain value into position zero of new array', function () {
+        // Arrange
+        let array = WatArray();
+
+        // Act
+        let actual = array.__(0, 4);
+
+        // Assert
+        expect(actual[0]).toBe(4);
+    });
 });
