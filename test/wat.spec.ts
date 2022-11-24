714d01ee47208e3c2f15da8a4174d4b1c8021b4d
kirypto <kirypto@gmail.com>
2022-11-24 17:08:35 -0600

    Properly import $_$ from test

--- test/wat.spec.ts
+++ test/wat.spec.ts
@@ -1,14 +1,11 @@
-// import assert = require("assert");
-//
-// const watjs = require("../src/wat.js");
+import {assert} from 'chai';
+import {$_$} from "../src/wat.js";
 
-import { assert } from 'chai';
 describe("Tests", () => {
     it('should run', () => {
         // Arrange
         // Act
         // Assert
-        assert.equal(4, 4);
+        assert.equal($_$, $_$);
     });
-
-})
+});
