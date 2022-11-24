af309a32cd662a9e88ac188c0b724ecefc2d27d2
kirypto <kirypto@gmail.com>
2022-11-24 14:53:07 -0600

    Move all code down into single wat.js

--- src/wat.js
+++ src/wat.js
@@ -1,5 +1,43 @@
 $_$ = {}
 
-$_$.$ = {}
+$_$.$ = (($__) => (() => {
+    $__ = {}
 
-$_$.$$ = {}
+    /**
+     * Ternary Operator
+     * @param _ Boolean, Conditional
+     * @param __ Any, True case return value
+     * @param _$ Any, False case return value
+     * @returns {*} Returns '`__`' if '`_`' is `true`, '`_$`' if '`__`' is `false`, and `undefined` otherwise
+     * @private
+     */
+    $__.__ = (_, __, _$) => (($___, ___, __$) => (() => {
+        ___ = [[][{}], __]
+        __$ = [[][{}], _$]
+
+        $___ = ___[(_ === !+[]) + ![]] || __$[(_ === ![]) + ![]] || [][{}]
+    })() || $___)()
+
+    /**
+     * Generic Iteration Function
+     * @param _ Any, Starting iteration value
+     * @param __ Function(*), A function that, given the current iteration value, returns true if iteration should continue
+     * @param _$ Function(*), A function that, given the current iteration value, returns the next iteration value
+     * @param ___ Function(*), A function that, given the current iteration value, performs some desired operation
+     */
+    $__.$ = (_, __, _$, ___) => ((_$_) => {
+        $_$.$.__(
+            __(_$_),
+            () => ___(_$_),
+            () => {
+            }
+        )()
+
+        $_$.$.__(
+            __(_$_),
+            () => $__.$(_$(_), __, _$,  ___),
+            () => {}
+        )()
+    })(_)
+})() || $__)()
+
