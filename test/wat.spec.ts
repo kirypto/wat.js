fe0f8999fc217035387f323b97e11f391c942531
kirypto <kirypto@gmail.com>
2022-11-24 17:02:45 -0600

    Modify tests to work with typescript

--- test/wat.spec.ts
+++ test/wat.spec.ts
@@ -1,10 +1,14 @@
+// import assert = require("assert");
+//
+// const watjs = require("../src/wat.js");
+
+import { assert } from 'chai';
 describe("Tests", () => {
     it('should run', () => {
         // Arrange
-
         // Act
-
         // Assert
+        assert.equal(4, 4);
     });
 
 })
