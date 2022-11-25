fba6b16e092776443f19a01fcd05e405f966ceae
kirypto <kirypto@gmail.com>
2022-11-25 12:22:57 -0600

    Add literal characters 'a' through 'c'

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ _A Truly Symbolic Language_
 
 
 Overhaul underway...
+
+## Development
+
+- Currently, the tests only work with node version `v15.8.0`.
--- test/wat.spec.ts
+++ test/wat.spec.ts
@@ -1,11 +1,42 @@
-import {assert} from 'chai';
+import {assert as chaiAssert} from 'chai';
 import {$_$} from "../src/wat.js";
 
-describe("Tests", () => {
-    it('should run', () => {
-        // Arrange
+// const wat = $_$;
+
+interface ChaiAssert {
+    typeOf: (actual: any, expectedType: string) => void;
+    equal: (actual: any, expected: any, message?: string) => void;
+}
+
+const assert: ChaiAssert = chaiAssert;
+
+function lpad(value: string, char: string, length: number): string {
+    return value.length >= length ? value : lpad(char + value, char, length);
+}
+
+describe(`Literals`, () => {
+    const literalContainer = "_";
+    it(`should have literal container object stored in '${literalContainer}'`, () => {
         // Act
+        // assert.equal(wat, 5);
+        const actual = $_$[literalContainer];
+
         // Assert
-        assert.equal($_$, $_$);
+        assert.typeOf(actual, "object");
     });
+
+    const supportedLetters = "abc";
+    for (const letter of supportedLetters) {
+        const key: string = lpad(letter.charCodeAt(0).toString(2), "0", 8)
+            .replaceAll("0", "_").replaceAll("1", "$");
+
+        it(`should have ${letter} stored at ${key}`, () => {
+            // Arrange
+            // Act
+            const actual = $_$[literalContainer][key];
+
+            // Assert
+            assert.equal(actual, letter);
+        });
+    }
 });
